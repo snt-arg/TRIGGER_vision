4283cf4fba43d1fd6b3b35831a0bd1d70520c470
Claudio <cima.claudio@gmail.com>
2023-06-08 18:35:42 +0200

    test yolo and cutler scripts

---
+++ obj_det_model/scripts/test_yolov8.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import torch as T
+from PIL import Image
+import cv2
+import os
+
+script_path = os.path.abspath(__file__)
+script_directory = os.path.dirname(script_path)
+
+model = YOLO(f'{script_directory}/../bottle_segmentation/YOLOv8m-512/weights/best.pt')  # load a pretrained model
+# Perform object detection on an image using the model
+# from PIL
+im1 = Image.open(f"{script_directory}/../datasets/roboflow_solidwaste/test/images/Test-Vid-bottles_mp4-312_jpg.rf.c26f7f45f13bb8877f6ae69427badd5f.jpg")
+# results = model.predict(source=im1, save=True, show=True)  # save plotted images
+res = model(im1)
+res_plotted = res[0].plot()
+cv2.imshow("result", res_plotted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
