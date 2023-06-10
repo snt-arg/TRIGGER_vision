1811588683c2fc2b64a659a91252246d3e47fd97
Claudio <cima.claudio@gmail.com>
2023-06-10 17:13:56 +0200

    aditional training args

--- obj_det_model/scripts/train_yolov8.py
+++ obj_det_model/scripts/train_yolov8.py
@@ -2,13 +2,21 @@ from comet_ml import Experiment
 from comet_ml.integration.pytorch import log_model
 from ultralytics import YOLO
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='YOLOv8 train CLI script')
-parser.add_argument('--net_size', type=str, choices=['m', 's', 'n'], default='n', help="Network size option")
+parser.add_argument('--net_size', type=str, choices=['m', 's', 'n'], default='m', help="Network size option")
+parser.add_argument('--dataset', type=str, default='data.yml', help="Dataset YAML file path")
 parser.add_argument('--input_size', type=int, default=416, help="Input image size (integer)")
 parser.add_argument('--workers', type=int, default=4, help="Num workers (cpu threads)")
+parser.add_argument('--epochs', type=int, default=150, help="Num of epochs")
+parser.add_argument('--save_period', type=int, default=20, help="Epochs before saving (-1 to disable)")
 parser.add_argument('--batch_size', type=int, default=32, help="Batch size")
-parser.add_argument('--lr0', type=float, default=1e-3, help="Init learning rate")
+parser.add_argument('--patience', type=int, default=10, help="Patience before early stopping")
+parser.add_argument('--optimizer', type=str, default='AdamW', choices=['SGD', 'Adam', 'Adamax', 'AdamW', 'NAdam', 'RAdam', 'RMSProp', 'auto'], help="Optimize Algorithm")
+parser.add_argument('--lr0', type=float, default=3e-3, help="Init learning rate")
+parser.add_argument('--lr_decay', type=float, default=5e-4, help="Learning rate constant decay rate")
+parser.add_argument('--warmup_epochs', type=float, default=5., help="Warmup epochs number (fraction ok)")
 parser.add_argument('--device', type=int, default=0, help="ID CUDA device")
 parser.add_argument('--api_key', type=str, default=argparse.SUPPRESS, help="COMETML API Key (personal use)")
 
@@ -23,7 +31,7 @@ if hasattr(args, 'imgsz'):
   try:
     experiment = Experiment(
       api_key = args.api_key,
-      project_name = f"{model_name}_{input_size}_segm_trash",
+      project_name = f"{model_name}_{input_size}_{Path(args.dataset).stem}_segm_trash",
       workspace="claudiocimarelli",
       auto_output_logging='default',
     )
@@ -34,21 +42,21 @@ if hasattr(args, 'imgsz'):
 # Report multiple hyperparameters using a dictionary:
 hyper_params = {
     "model": f"{model_name}.pt",
-    "data": 'data.yaml', # path to data file, i.e. coco128.yaml
-    "epochs": 500, # number of epochs to train for
-    "patience": 15, # epochs to wait for no observable improvement for early stopping of training
+    "data": args.dataset, # path to data file, i.e. coco128.yaml
+    "epochs": args.epochs, # number of epochs to train for
+    "patience": args.patience, # epochs to wait for no observable improvement for early stopping of training
     "batch": args.batch_size, # number of images per batch (-1 for AutoBatch)
     "imgsz": input_size, # size of input images as integer or w,h
     "save": True, # save train checkpoints and predict results
-    "save_period": 20, # Save checkpoint every x epochs (disabled if < 1)
+    "save_period": args.save_period, # Save checkpoint every x epochs (disabled if < 1)
     "cache": True, # True/ram, disk or False. Use cache for data loading
     "device": args.device, # device to run on, i.e. cuda device=0 or device=0,1,2,3 or device=cpu
     "workers": args.workers, # number of worker threads for data loading (per RANK if DDP)
     "project": 'bottle_segmentation', # project name
-    "name": f"{model_name}_{input_size}",# experiment name, results saved to 'project/name' directory
+    "name": f"{model_name}_{input_size}_{Path(args.dataset).stem}",# experiment name, results saved to 'project/name' directory
     "exist_ok": False, # whether to overwrite existing experiment
     "pretrained": True, # whether to use a pretrained model
-    "optimizer": 'AdamW', # optimizer to use, choices=[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]
+    "optimizer": args.optimizer, # optimizer to use, choices=[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]
     "verbose": True, # whether to print verbose output
     "seed": 0, # random seed for reproducibility
     "deterministic": True, # whether to enable deterministic mode
@@ -63,8 +71,8 @@ hyper_params = {
     "lr0": args.lr0, # initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
     "lrf": 0.01, # final learning rate (lr0 * lrf)
     "momentum": 0.9, # SGD momentum/Adam beta1
-    "weight_decay": 0.0005, # optimizer weight decay 5e-4
-    "warmup_epochs": 5.0, # warmup epochs (fractions ok)
+    "weight_decay": args.lr_decay, # optimizer weight decay 5e-4
+    "warmup_epochs": args.warmup_epochs, # warmup epochs (fractions ok)
     "warmup_momentum": 0.8, # warmup initial momentum
     "warmup_bias_lr": 0.01, # warmup initial bias lr
     # "box: 7.5 # box loss gain
@@ -84,7 +92,7 @@ hyper_params = {
     # "perspective": 0.00001, # image perspective (+/- fraction), range 0-0.001
     "flipud": 0.5, # image flip up-down (probability)
     "fliplr": 0.5, # image flip left-right (probability)
-    # "mosaic: 1.0 # image mosaic (probability)
+    "mosaic": .1 # image mosaic (probability)
     # "mixup: 0.0 # image mixup (probability)
     # "copy_paste: 0.0 # segment copy-paste (probability)
 }
