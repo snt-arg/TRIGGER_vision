7db63de8a604ccf6d0bbf640f9dbd95f4d0fe63e
Claudio <cima.claudio@gmail.com>
2023-06-11 11:37:26 +0200

    moved evaluation here

--- obj_det_model/scripts/test_yolov8.py
+++ obj_det_model/scripts/test_yolov8.py
@@ -8,6 +8,8 @@ script_path = os.path.abspath(__file__)
 script_directory = os.path.dirname(script_path)
 
 model = YOLO(f'{script_directory}/../bottle_segmentation/YOLOv8m-512/weights/best.pt')  # load a pretrained model
+# Evaluate the model's performance on the validation set
+results = model.val()
 # Perform object detection on an image using the model
 # from PIL
 im1 = Image.open(f"{script_directory}/../datasets/roboflow_solidwaste/test/images/Test-Vid-bottles_mp4-312_jpg.rf.c26f7f45f13bb8877f6ae69427badd5f.jpg")
